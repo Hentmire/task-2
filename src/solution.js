8f7eea0a665dfc2682fe1dd75cc309fec3968a02
Victoria Udodova <hentmire@gmail.com>
2019-02-09 18:36:37 +0300

    feat: add solution function

--- src/solution.js
+++ src/solution.js
@@ -1,3 +1,28 @@
 const solution = function(graph, start, finish)  {
-    // Ваше решение
+    const costs = {}, parents = {}, solution = {};
+    const rightPath = [];
+    
+    costs[start] = 0;
+    for (let node in graph) {
+        for (let neighbour in graph[node]) {
+            let newCost = costs[node] + graph[node][neighbour];
+            if (costs[neighbour]) {
+                if (costs[neighbour] > newCost) {
+                    costs[neighbour] = newCost;
+                    parents[neighbour] = node;
+                }
+            } else {
+                costs[neighbour] = newCost;
+                parents[neighbour] = node;
+            }            
+        }        
+    }
+    let parent = finish;
+    while (parent) {
+        rightPath.unshift(parent);
+        parent = parents[parent];
+    }
+    solution.distance = costs[finish];
+    solution.path = rightPath;
+    return solution;
 }
